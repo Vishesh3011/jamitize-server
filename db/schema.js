a39a39840532b16de8ee0344e88fde225bd67ae0
Vishesh Tarun Modi <u8186280@anu.edu.au>
2025-07-28 15:47:34 +1000

    added some schema designs for mongodb

---
+++ db/schema.js
@@ -0,0 +1,37 @@
+use jamitize;
+
+const collections = db.getCollectionNames();
+collections.forEach(function(collName) {
+    db[collName].drop();
+    print("Dropped collection: " + collName);
+});
+
+db.createCollection("users");
+db.createCollection("sessions");
+db.createCollection("messages");
+
+db.users.insertMany([
+    {
+        name: "Vishesh Modi",
+        instrument: "Guitar",
+        genres: ["Indie", "Pop"],
+        city: "Sydney",
+        experience: 3,
+        availableForJam: true,
+        bio: "Looking to jam on weekends.",
+        socials: {
+            instagram: "@vjam",
+            youtube: "youtube.com/vjam"
+        }
+    },
+    {
+        name: "Alex Johnson",
+        instrument: "Drums",
+        genres: ["Rock", "Jazz"],
+        city: "Melbourne",
+        experience: 5,
+        availableForJam: false
+    }
+]);
+
+print("Setup complete!");
